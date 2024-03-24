d589afc2671e1294de107e7fd12ae4a9e06b9f08
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-03-25 03:38:25 +0530

    completed the hero section

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -2,7 +2,7 @@ import heroimage from "../assets/images/hero-image.png";
 const Home = () => {
   return (
     <div className="home row m-0 p-0">
-      <div className="hero-section row m-0 p-5 pb-0">
+      <div className="hero-section row m-0 p-5 pb-0 align-items-center ">
           <div className="col-6">
             <h1 className="m-0 p-0 text-light">
               <span style={{ color: "#F48C06" }}>Studying</span> Online is now
@@ -11,8 +11,10 @@ const Home = () => {
             <p className="m-0 p-0 text-light fs-4 mt-5">
                 NextGenEdu is an interesting platform that will teach you in more interactive way
             </p>
-            <div className="row m-0 p-0">
-                <button className=" join btn rounded-pill">Join for free</button>
+            <div className="row m-0 p-0 mt-5 align-items-center ">
+                <button className=" join btn rounded-pill text-light fw-medium fs-4 w-auto p-5 pt-2 pb-2">Join for free</button>
+                <i class="bi bi-play-circle-fill w-auto text-light ms-4"></i>
+                <p className="w-auto m-0 p-0 ms-2 fs-5">watch how it works</p>
             </div>
           </div>
 
