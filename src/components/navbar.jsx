31b27308dcf067e5e57ec5acfbc9d18ccc64a3b7
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-03-25 20:08:25 +0530

    adding the offcanvas

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,33 +3,135 @@ import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.png";
 const Navbar = () => {
   return (
-    <div className="Navbar row m-0 p-0 justify-content-between align-items-center p-5 pt-3 pb-3">
-      <div className="col-2 overflow-hidden ">
-        <img src={logo} alt="logo" className="w-75" />
+    <div className="Navbar row m-0 p-0 justify-content-between align-items-center pt-3 pb-3 p-md-5 pt-md-3 pb-md-3 ">
+      <div className="col-4 col-lg-2 overflow-hidden ">
+        <img src={logo} alt="logo" className="w-lg-75 w-100" />
       </div>
-      <div className="col-8">
+      <div className="col-8 d-lg-flex d-none">
         <div className="row m-0 p-0 align-items-center">
-          <Link to="/" className=" text-light w-auto m-0 p-0 text-decoration-none ms-5 me-5 ">
+          <Link
+            to="/"
+            className=" text-light w-auto m-0 p-0 text-decoration-none ms-5 me-5 "
+          >
             <p className="m-0 p-0 w-auto">Home</p>
           </Link>
-          <Link to="/courses" className="text-light w-auto m-0 p-0 text-decoration-none ms-5 me-5">
+          <Link
+            to="/courses"
+            className="text-light w-auto m-0 p-0 text-decoration-none ms-5 me-5"
+          >
             <p className="m-0 p-0 w-auto">Courses</p>
           </Link>
-          <Link to="/careers" className="text-light w-auto m-0 p-0 text-decoration-none ms-5 me-5">
+          <Link
+            to="/careers"
+            className="text-light w-auto m-0 p-0 text-decoration-none ms-5 me-5"
+          >
             <p className="m-0 p-0 w-auto">Careers</p>
           </Link>
-          <Link to="/blog" className="text-light w-auto m-0 p-0 text-decoration-none ms-5 me-5">
+          <Link
+            to="/blog"
+            className="text-light w-auto m-0 p-0 text-decoration-none ms-5 me-5"
+          >
             <p className="m-0 p-0 w-auto">Blog</p>
           </Link>
-          <Link to="/aboutUs" className="text-light w-auto m-0 p-0 text-decoration-none ms-4 me-5">
+          <Link
+            to="/aboutUs"
+            className="text-light w-auto m-0 p-0 text-decoration-none ms-4 me-5"
+          >
             <p className="m-0 p-0 w-auto">About Us</p>
           </Link>
-          <Link to="/login" className="text-light w-auto m-0 p-0 text-decoration-none ms-4 me-2">
-            <button className="login btn bg-light rounded-pill ps-4 pe-4">Login</button>
+          <Link
+            to="/login"
+            className="text-light w-auto m-0 p-0 text-decoration-none ms-4 me-2"
+          >
+            <button className="login btn bg-light rounded-pill ps-4 pe-4">
+              Login
+            </button>
           </Link>
-          <Link to="/SignUp" className="text-light w-auto m-0 p-0 text-decoration-none ms-2">
-            <button className="signup btn rounded-pill ps-3 pe-3 text-light">Sign Up</button>
+          <Link
+            to="/SignUp"
+            className="text-light w-auto m-0 p-0 text-decoration-none ms-2"
+          >
+            <button className="signup btn rounded-pill ps-3 pe-3 text-light">
+              Sign Up
+            </button>
           </Link>
+        </div>
+      </div>
+
+      <i
+        class="bi bi-list fs-1 w-auto d-flex d-lg-none"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasRight"
+        aria-controls="offcanvasRight"
+      ></i>
+
+      <div
+        class="offcanvas offcanvas-end"
+        tabindex="-1"
+        id="offcanvasRight"
+        aria-labelledby="offcanvasRightLabel"
+      >
+        <div class="offcanvas-header">
+          <div className="col-6">
+            <img src={logo} alt="logo" className="w-100" />
+          </div>
+
+          <button
+            type="button"
+            class="btn-close text-reset"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body">
+          <div className="row m-0 p-0 align-items-center">
+            <Link
+              to="/"
+              className=" text-light  m-0 p-0 text-decoration-none mt-3 mb-3 "
+            >
+              <p className="m-0 p-0 w-auto fs-4 ">Home</p>
+            </Link>
+            <Link
+              to="/courses"
+              className="text-light  m-0 p-0 text-decoration-none mt-3 mb-3"
+            >
+              <p className="m-0 p-0 w-auto fs-4 ">Courses</p>
+            </Link>
+            <Link
+              to="/careers"
+              className="text-light  m-0 p-0 text-decoration-none mt-3 mb-3"
+            >
+              <p className="m-0 p-0 w-auto fs-4 ">Careers</p>
+            </Link>
+            <Link
+              to="/blog"
+              className="text-light  m-0 p-0 text-decoration-none mt-3 mb-3"
+            >
+              <p className="m-0 p-0 w-auto fs-4 ">Blog</p>
+            </Link>
+            <Link
+              to="/aboutUs"
+              className="text-light  m-0 p-0 text-decoration-none mt-3 mb-3"
+            >
+              <p className="m-0 p-0 w-auto fs-4 ">About Us</p>
+            </Link>
+            <Link
+              to="/login"
+              className="text-light w-auto m-0 p-0 text-decoration-none"
+            >
+              <button className="login btn bg-light rounded-pill ps-4 pe-4 fs-4">
+                Login
+              </button>
+            </Link>
+            <Link
+              to="/SignUp"
+              className="text-light w-auto m-0 p-0 text-decoration-none ms-2"
+            >
+              <button className="signup btn rounded-pill ps-3 pe-3 text-light fs-4">
+                Sign Up
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -2,8 +2,8 @@ import heroimage from "../assets/images/hero-image.png";
 const Home = () => {
   return (
     <div className="home row m-0 p-0">
-      <div className="hero-section row m-0 p-5 pb-0 align-items-center ">
-          <div className="col-6">
+      <div className="hero-section row m-0 p-lg-5  pt-5 pt-lg-0pb-0 align-items-center ">
+          <div className="col-lg-6">
             <h1 className="m-0 p-0 text-light">
               <span style={{ color: "#F48C06" }}>Studying</span> Online is now
               much easier
@@ -13,13 +13,13 @@ const Home = () => {
             </p>
             <div className="row m-0 p-0 mt-5 align-items-center ">
                 <button className=" join btn rounded-pill text-light fw-medium fs-4 w-auto p-5 pt-2 pb-2">Join for free</button>
-                <i class="bi bi-play-circle-fill w-auto text-light ms-4"></i>
+                <i class="bi bi-play-circle-fill w-auto text-light ms-lg-4"></i>
                 <p className="w-auto m-0 p-0 ms-2 fs-5">watch how it works</p>
             </div>
           </div>
 
-          <div className="col-6">
-            <img src={heroimage} alt="hero image" className="w-100" />
+          <div className="col-lg-6">
+            <img src={heroimage} alt="hero image" className="w-100 h-100" />
           </div>
         
       </div>
