6610f3052cf2c4f86531b7bef73a2721469e36c3
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-03-30 02:54:28 +0530

    adding the what is nextgen edu in home page

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,7 +1,9 @@
-import heroimage from "../assets/images/hero-image.png";
+import { Link } from "react-router-dom";
+
 import CourseCategoryItem from "../components/courseCategoryItem";
 import CourseItems from "../components/courseItem";
-import { Link } from "react-router-dom";
+
+import heroimage from "../assets/images/hero-image.png";
 const courses = [
   {
     id: 1,
@@ -154,7 +156,7 @@ const Home = () => {
       </div>
 
       {/**something about nextgenedu */}
-      <div className="m-0 p-0">
+      <div className="m-0 p-0 mt-5 mb-5">
         <div className="container">
           <h2 className="m-0 p-0 text-center" style={{ color: "#2F327D" }}>
             All-In-One{" "}
@@ -166,18 +168,113 @@ const Home = () => {
             NextGenEdu is one powerful online software suite that combines all
             the tools <br /> needed to run a successful school or office
           </p>
-          <div className="courseCategoryItem col-lg-3 p-4">
-            <div className="card  p-3 border-0 justify-content-center align-items-center rounded-4">
-                <div className="icon d-flex justify-content-center align-items-center w-auto rounded-2" style={{backgroundColor:'##00CBB8'}}>
-                    <i className={`bi bi-arrow w-auto fs-4 `} style={{color:'##00CBB8'}}></i>
+
+          <div className="row m-0 p-0 mt-5 pt-3 justify-content-center ">
+            <div className="courseCategoryItem col-lg-3 p-3 mt-lg-0 mt-4  ">
+              <div className="card  p-3 pt-5 border-0 justify-content-center align-items-center rounded-4">
+                <div
+                  className="upper-icon position-absolute  d-flex justify-content-center align-items-center w-auto rounded-circle"
+                  style={{ backgroundColor: "#5B72EE" }}
+                >
+                  <i
+                    className={`bi bi-receipt w-auto fs-4 `}
+                    style={{ color: "#fff" }}
+                  ></i>
                 </div>
-                <h5 className="m-0 p-0 mt-3">Online</h5>
-                <p className="m-0 p-0 mt-4 mb-2 text-center">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam earum autem doloremque ducimus 
+                <h5
+                  className="m-0 p-0 mt-4 text-center"
+                  style={{ color: "#2F327D" }}
+                >
+                  Online Billing, Invoicing, & Contracts
+                </h5>
+                <p className="m-0 p-0 mt-4 mb-2 text-center text-secondary">
+                  Simple and secure control of your organization’s financial and
+                  legal transactions. Send customized invoices and contracts{" "}
                 </p>
+              </div>
             </div>
+
+            <div className="courseCategoryItem col-lg-3 p-3 mt-lg-0 mt-4 ">
+              <div className="card  p-3 pt-5 border-0 justify-content-center align-items-center rounded-4">
+                <div
+                  className="upper-icon position-absolute  d-flex justify-content-center align-items-center w-auto rounded-circle"
+                  style={{ backgroundColor: "#00CBB8" }}
+                >
+                  <i
+                    className={`bi bi-calendar2-week-fill w-auto fs-4 `}
+                    style={{ color: "F5F5FC" }}
+                  ></i>
+                </div>
+                <h5
+                  className="m-0 p-0 mt-4 text-center"
+                  style={{ color: "#2F327D" }}
+                >
+                  Easy Scheduling & Attendance Tracking
+                </h5>
+                <p className="m-0 p-0 mt-4 mb-2 text-center text-secondary">
+                  Schedule and reserve classrooms at one campus or multiple
+                  campuses. Keep detailed records of student attendance{" "}
+                </p>
+              </div>
+            </div>
+
+            <div className="courseCategoryItem col-lg-3 p-3 mt-lg-0 mt-4 ">
+              <div className="card  p-3  pt-5 border-0 justify-content-center align-items-center rounded-4">
+                <div
+                  className="upper-icon position-absolute  d-flex justify-content-center align-items-center w-auto rounded-circle"
+                  style={{ backgroundColor: "#29B9E7" }}
+                >
+                  <i
+                    className={`bi bi-people-fill w-auto fs-4 `}
+                    style={{ color: "#fff" }}
+                  ></i>
+                </div>
+                <h5
+                  className="m-0 p-0 mt-5 text-center"
+                  style={{ color: "#2F327D" }}
+                >
+                  Customer Tracking
+                </h5>
+                <p className="m-0 p-0 mt-4 mb-2 text-center text-secondary">
+                  Simple and secure control of your organization’s financial and
+                  legal transactions. Send customized invoices and contracts{" "}
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-          
+      </div>
+
+      {/* what is nextgenEdu*/}
+      <div className="m-0 p-0 justify-content-center ">
+        <div className="container justify-content-center align-items-center   d-flex flex-column py-5">
+          <h2 className="m-0 p-0 text-center" style={{ color: "#2F327D" }}>
+            What is{" "}
+            <span className="" style={{ color: "#00CBB8" }}>
+              NextGenEdu
+            </span>
+          </h2>
+          <p className="m-0 p-0 mt-3 text-secondary text-center col-8">
+            NextGenEdu is a platform that allows educators to create online classes
+            whereby they can store the course materials online; manage
+            assignments, quizzes and exams; monitor due dates; grade results and
+            provide students with feedback all in one place.{" "}
+          </p>
+
+          <div className="what-nextGenEdu row m-0 p-0 mt-5 w-100 justify-content-center">
+            <div className="col-lg-5 p-4">
+              <div className="card border-0 rounded-4 overflow-hidden justify-content-center align-items-center">
+                    <h4 className="m-0 p-0 text-white z-2 ">FOR INSTRUCTORS</h4>
+                    <button className="btn btn-transparent border border-2 rounded-pill w-auto z-2 text-light mt-3">Start a class today</button>
+              </div>
+            </div>
+            <div className="col-lg-5 p-4">
+              <div className="card border-0 rounded-4 overflow-hidden justify-content-center align-items-center">
+                    <h4 className="m-0 p-0 text-white z-2 ">FOR STUDENTS</h4>
+                    <button className="btn btn-transparent border border-2 rounded-pill w-auto z-2 text-light mt-3">Enter access code</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
