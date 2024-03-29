cb1712982cad0dd9af121547b72e77b4ecfd4c06
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-03-29 16:21:09 +0530

    adding the course by category list in home

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,5 +1,7 @@
 import heroimage from "../assets/images/hero-image.png";
+import CourseCategoryItem from "../components/courseCategoryItem";
 import CourseItems from "../components/courseItem";
+import { Link } from "react-router-dom";
 const courses = [
   {
     id: 1,
@@ -45,6 +47,7 @@ const Home = () => {
           <img src={heroimage} alt="hero image" className="w-100 h-100" />
         </div>
       </div>
+      {/* course list */}
       <div className="courses-section py-5 d-flex flex-column align-items-center  ">
         <h1 className="m-0 p-0 text-center">Featured Courses</h1>
         <p className="m-0 p-0 text-center text-secondary w-lg-50 fs-5 mt-3">
@@ -52,11 +55,102 @@ const Home = () => {
           Eduflow to facilitate learning experiences no matter the context.
         </p>
         <div className="container mt-5">
+          <div className="row m-0 p-0 justify-content-between mt-4 mb-4">
+            <h5 className="m-0 p-0 w-auto">Get choice of your course</h5>
+            <Link
+              to="/courses"
+              className="w-auto text-decoration-none fw-medium"
+              style={{ color: "#49BBBD" }}
+            >
+              see all
+            </Link>
+          </div>
           <div className="row">
             <CourseItems />
             <CourseItems />
             <CourseItems />
             <CourseItems />
+          </div>
+        </div>
+      </div>
+      <div
+        className="pt-3 pb-5"
+        style={{ backgroundColor: "rgba(157,204,255,0.2)" }}
+      >
+        <div className="container mt-5 mb-5">
+          <div className="row m-0 p-0 justify-content-between mt-4 mb-4">
+            <h5 className="m-0 p-0 w-auto">Recommended for you</h5>
+            <Link
+              to="/courses"
+              className="w-auto text-decoration-none fw-medium"
+              style={{ color: "#49BBBD" }}
+            >
+              see all
+            </Link>
+          </div>
+          <div className="row">
+            <CourseItems />
+            <CourseItems />
+            <CourseItems />
+            <CourseItems />
+          </div>
+        </div>
+      </div>
+
+
+      {/**course category list */}
+      <div className="courseCategoryList">
+        <div className="container  mt-5 mb-5">
+          <div className="row m-0 p-0">
+            <CourseCategoryItem
+              name="Design"
+              iconName="brush-fill"
+              iconColor="#49BBBD"
+              iconBgColor="rgba(73,187,189,0.3)"
+            />
+            <CourseCategoryItem
+              name="Development"
+              iconName="display"
+              iconColor="#5B72EE"
+              iconBgColor="rgba(91,114,238,0.3)"
+            />
+            <CourseCategoryItem
+              name="Web Development"
+              iconName="database-fill"
+              iconColor="#9DCCFF"
+              iconBgColor="rgba(157,204,255,0.3)"
+            />
+            <CourseCategoryItem
+              name="Business"
+              iconName="briefcase"
+              iconColor="#00CBB8"
+              iconBgColor="rgba(0,203,184,0.3)"
+            />
+             <CourseCategoryItem
+              name="Marketing"
+              iconName="shop-window"
+              iconColor="#F48C06"
+              iconBgColor="rgba(244,140,6,0.3)"
+            />
+            <CourseCategoryItem
+              name="Photography"
+              iconName="camera-fill"
+              iconColor="#EE645B"
+              iconBgColor="rgba(238,100,91,0.3)"
+            />
+            <CourseCategoryItem
+              name="Acting"
+              iconName="film"
+              iconColor="#252641"
+              iconBgColor="rgba(37,38,65,0.3)"
+            />
+            <CourseCategoryItem
+              name="Business"
+              iconName="brush-fill"
+              iconColor="#00CBB8"
+              iconBgColor="rgba(0,203,184,0.3)"
+            />
+           
           </div>
         </div>
       </div>
