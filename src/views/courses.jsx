8b71444fc5c02c9fa23f57c0766f76a9c7037a43
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-03-31 20:20:58 +0530

    complete the course page

--- src/views/courses.jsx
+++ src/views/courses.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 import CourseCategoryItem from "../components/courseCategoryItem";
 import CourseItems from "../components/courseItem";
-import ProgressCourseItem from '../components/progressCourseItem';
+import ProgressCourseItem from "../components/progressCourseItem";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import {   Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 
 // import required modules
-import { Pagination } from "swiper/modules";
+import {Autoplay, Pagination } from "swiper/modules";
 
 const Courses = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
@@ -36,26 +36,64 @@ const Courses = () => {
             <Swiper
               slidesPerView={3}
               spaceBetween={30}
+              loop={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
               pagination={{
                 clickable: true,
-                className: "swiper-pagination-top-spacing"
+                className: "swiper-pagination-top-spacing",
               }}
-              modules={[Pagination]}
+              modules={[Autoplay,Pagination]}
+              breakpoints={{
+                // When window width is <= 640px, show only 1 slide
+                640: {
+                  slidesPerView: 1,
+                },
+                // When window width is <= 768px, show 2 slides
+                768: {
+                  slidesPerView: 2,
+                },
+              }}
               className="mySwiper"
             >
-              <SwiperSlide><ProgressCourseItem/></SwiperSlide>
-              <SwiperSlide><ProgressCourseItem/></SwiperSlide>
-              <SwiperSlide><ProgressCourseItem/></SwiperSlide>
-              <SwiperSlide><ProgressCourseItem/></SwiperSlide>
-              <SwiperSlide><ProgressCourseItem/></SwiperSlide>
-              <SwiperSlide><ProgressCourseItem/></SwiperSlide>
-              <SwiperSlide><ProgressCourseItem/></SwiperSlide>
-              <SwiperSlide><ProgressCourseItem/></SwiperSlide>
-              <SwiperSlide><ProgressCourseItem/></SwiperSlide>
-              <SwiperSlide><ProgressCourseItem/></SwiperSlide>
-              <SwiperSlide><ProgressCourseItem/></SwiperSlide>
-              <SwiperSlide><ProgressCourseItem/></SwiperSlide>
-             
+              <SwiperSlide>
+                <ProgressCourseItem />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ProgressCourseItem />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ProgressCourseItem />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ProgressCourseItem />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ProgressCourseItem />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ProgressCourseItem />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ProgressCourseItem />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ProgressCourseItem />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ProgressCourseItem />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ProgressCourseItem />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ProgressCourseItem />
+              </SwiperSlide>
+              <SwiperSlide>
+                <ProgressCourseItem />
+              </SwiperSlide>
             </Swiper>
           </div>
         </div>
@@ -117,31 +155,6 @@ const Courses = () => {
         </div>
       </div>
       {/* course list */}
-      <div className="courses-section py-5 d-flex flex-column align-items-center  ">
-        <h1 className="m-0 p-0 text-center">Featured Courses</h1>
-        <p className="m-0 p-0 text-center text-secondary w-lg-50 fs-5 mt-3">
-          Learning often happens in classrooms but it doesn’t have to. Use
-          Eduflow to facilitate learning experiences no matter the context.
-        </p>
-        <div className="container mt-5">
-          <div className="row m-0 p-0 justify-content-between mt-4 mb-4">
-            <h5 className="m-0 p-0 w-auto">Get choice of your course</h5>
-            <Link
-              to="/courses"
-              className="w-auto text-decoration-none fw-medium"
-              style={{ color: "#49BBBD" }}
-            >
-              see all
-            </Link>
-          </div>
-          <div className="row">
-            <CourseItems />
-            <CourseItems />
-            <CourseItems />
-            <CourseItems />
-          </div>
-        </div>
-      </div>
       <div
         className="pt-3 pb-5"
         style={{ backgroundColor: "rgba(157,204,255,0.2)" }}
@@ -165,6 +178,89 @@ const Courses = () => {
           </div>
         </div>
       </div>
+      <div className="courses-section py-5 d-flex flex-column align-items-center  ">
+       
+        <div className="container mt-5">
+          <div className="row m-0 p-0 justify-content-between mt-4 mb-4">
+            <h5 className="m-0 p-0 w-auto">Get choice of your course</h5>
+            <Link
+              to="/courses"
+              className="w-auto text-decoration-none fw-medium"
+              style={{ color: "#49BBBD" }}
+            >
+              see all
+            </Link>
+          </div>
+          <div className="row">
+            <CourseItems />
+            <CourseItems />
+            <CourseItems />
+            <CourseItems />
+          </div>
+        </div>
+      </div>
+
+      {/**online coaching remote */}
+      <div className=" m-0 mt-5 p-0 d-flex justify-content-center ">
+        <div className="learningnow container">
+            <div className="card p-5 rounded-4 d-flex align-items-center  justify-content-center " style={{backgroundColor:'#252641'}}>
+                <h4 className="m-0 p-0 w-auto text-light">Online coaching lessons for remote learning.</h4>
+                <p className="m-0 p-0 mt-3 text-white text-center ">
+                    Join our online classroom and learn from experts in the field. Our courses are designed to be completed at your own pace, allowing
+                    Join our online classroom and get access to a library of video tutorials, quizzes, and assessments. Our experienced train
+                    Join our online classes and get access to a library of video tutorials, live sessions, and resources. Our experienced trainers will guide
+                </p>
+                <button className=" btn text-light mt-4">
+                    start learning now
+                </button>
+            </div>
+        </div>
+      </div>
+      <div className="courses-section py-5 d-flex flex-column align-items-center  ">
+       
+       <div className="container mt-5">
+         <div className="row m-0 p-0 justify-content-between mt-4 mb-4">
+           <h5 className="m-0 p-0 w-auto">The course in personal development</h5>
+           <Link
+             to="/courses"
+             className="w-auto text-decoration-none fw-medium"
+             style={{ color: "#49BBBD" }}
+           >
+             see all
+           </Link>
+         </div>
+         <div className="row">
+           <CourseItems />
+           <CourseItems />
+           <CourseItems />
+           <CourseItems />
+         </div>
+       </div>
+     </div>
+     <div
+        className="pt-3 pb-5"
+        style={{ backgroundColor: "rgba(157,204,255,0.2)" }}
+      >
+        <div className="container mt-5 mb-5">
+          <div className="row m-0 p-0 justify-content-between mt-4 mb-4">
+            <h5 className="m-0 p-0 w-auto">Student are viewing</h5>
+            <Link
+              to="/courses"
+              className="w-auto text-decoration-none fw-medium"
+              style={{ color: "#49BBBD" }}
+            >
+              see all
+            </Link>
+          </div>
+          <div className="row">
+            <CourseItems />
+            <CourseItems />
+            <CourseItems />
+            <CourseItems />
+          </div>
+        </div>
+      </div>
+     
     </div>
   );
 };
