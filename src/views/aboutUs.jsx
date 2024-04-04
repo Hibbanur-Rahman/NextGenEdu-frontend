1b2671efd71bd6c8aae1c652b622b6b67bfd855a
HIBBANUR RAHMAN <hibbanrahmanhyt@gmail.com>
2024-04-05 01:17:46 +0530

    adding the about us

--- src/views/aboutUs.jsx
+++ src/views/aboutUs.jsx
@@ -1,9 +1,14 @@
-const AboutUs=()=>{
-    return(
-        <div className="row m-0 p-0 mt-5 pt-5">
-            <h1 className="about us"></h1>
-        </div>
-    )
-}
+const AboutUs = () => {
+  return (
+    <div className="aboutUs row m-0 p-0 mt-5 pt-5">
+      <div className="hero d-flex flex-column  align-items-center justify-content-center">
+       <h1 className="text-light text-center m-0 p-0 mb-3">Our Story</h1>
+        <p className="m-0 p-0 text-light text-center fs-5">
+        Make learning and teaching more effective with active participation and student collaboration
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default AboutUs;
